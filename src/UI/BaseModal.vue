7068166fb259ab46189ebcea48b77565f0edce5d
denzel <denzelvojn@gmail.com>
2025-08-04 19:32:27 +0300

    Анимация модального окна

--- src/UI/BaseModal.vue
+++ src/UI/BaseModal.vue
@@ -1,36 +1,64 @@
 <script setup>
 import BaseModalButtonClose from "./BaseModalButtonClose.vue";
+import BaseModalOverlay from "./BaseModalOverlay.vue";
 import {onMounted, ref} from "vue";
 
 const emit = defineEmits(["close"]);
 const wrapperRef = ref(null);
 
+const isModalOpen = ref(true);
+
+const close = () => {
+  isModalOpen.value = false;
+};
+
 onMounted(() => {
-  wrapperRef.value?.focus()
-})
+  wrapperRef.value?.focus();
+});
 </script>
 
 <template>
   <div
-      class="fixed inset-0 z-10 bg-black/80 focus:outline-none"
-      @click.self="emit('close')"
-      @keydown.esc="emit('close')"
+      class="fixed inset-0 z-10 focus:outline-none"
+      @keydown.esc="close"
       tabindex="-1"
       ref="wrapperRef"
   >
-    <div class="bg-white max-w-sm mx-auto my-8">
-      <div class="p-2 text-right">
-        <BaseModalButtonClose @click="emit('close')"/>
+    <transition
+        appear
+        enter-active-class="duration-200"
+        enter-from-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-active-class="duration-200"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+    >
+      <BaseModalOverlay v-if="isModalOpen" @click="close"/>
+    </transition>
+
+    <transition
+        appear
+        enter-active-class="duration-200 transform ease-out"
+        enter-from-class="opacity-0 translate-y-4"
+        enter-to-class="opacity-100 translate-y-0"
+        leave-active-class="duration-200 transform ease-in"
+        leave-from-class="opacity-100 translate-y-0"
+        leave-to-class="opacity-0 translate-y-4"
+        @after-leave="emit('close')"
+    >
+      <div
+          v-if="isModalOpen"
+          class="bg-white max-w-sm mx-auto my-8 relative"
+      >
+        <div class="p-2 text-right">
+          <BaseModalButtonClose @click="close"/>
+        </div>
+        <div class="p-6">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci, aspernatur aut cupiditate
+          dolorem doloribus earum eum ex facere inventore laborum natus officia perspiciatis quo saepe sequi temporibus.
+          Alias, dignissimos?
+        </div>
       </div>
-      <div class="p-6">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab autem consectetur dolorum earum facilis laboriosam,
-        minima molestiae, nobis perferendis quidem vel vitae, voluptas? Consectetur illo modi molestiae molestias
-        nostrum velit?
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/UI/BaseModalOverlay.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="fixed inset-0 bg-black/80"></div>
+</template>
+
+<style scoped>
+
+</style>
